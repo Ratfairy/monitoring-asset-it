06d84fd5616b20435d0e21ace0a5e432f6c5123c
Ratfairy <manaotongku@gmail.com>
2026-05-07 09:22:17 +0700

    qr ke public-show

--- resources/views/assets/print.blade.php
+++ resources/views/assets/print.blade.php
@@ -34,7 +34,7 @@
 
     <div style="margin:20px 0;">
 
-        {!! QrCode::size(200)->generate(route('assets.show', $asset->id)) !!}
+        {!! QrCode::size(200)->generate(route('assets.public-show', $asset->id)) !!}
 
     </div>
 
